6dd4b090efa510a77b9474124f1f7fd67c6206e2
Zanqueta <matheuszanqueta20@gmail.com>
2025-07-21 20:35:31 -0300

    Finalizando o ex 2

--- src/ex2.java
+++ src/ex2.java
@@ -1,5 +1,32 @@
+import java.util.Scanner;
+
 public class ex2 {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        var scanner = new Scanner(System.in);
+        
+        System.out.println("Cálculo de IMC");
+        System.out.println("---");
+
+        System.out.println("Digite sua altura:");
+        var altura = scanner.nextFloat();
+
+        System.out.println("Digite seu peso:");
+        var peso = scanner.nextFloat();
+
+        var imc = peso / (altura * altura);
+
+        if(imc <= 18.5){
+            System.out.printf("Abaixo do peso");
+        }else if (imc <= 24.9) {
+            System.out.printf("Peso ideal");
+        }else if(imc <= 29.9){
+            System.out.printf("Levemente acima do peso");
+        }else if (imc <= 34.9) {
+            System.out.printf("Obesidade Grau I");
+        }else if (imc <= 39.9) {
+            System.out.printf("Obesidade Grau II (Severa)");
+        }else{
+            System.out.printf("Obesidade Grau III (Mórbida)");
+        }
     }
 }
