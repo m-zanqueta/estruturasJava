ee5d0529225b854af9e8d762ec79700ba2b8aab4
Zanqueta <matheuszanqueta20@gmail.com>
2025-07-21 20:22:15 -0300

    finalizando o ex 1

---
+++ src/ex1.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class ex1 {
+    public static void main(String[] args) throws Exception {
+        var scanner = new Scanner(System.in);
+        
+        System.out.println("Tabuada automática!");
+        System.out.println("---");
+
+        System.out.println("Digite um número:");
+        var numero = scanner.nextInt();
+
+        for (var i = 1; i <= 10; i++){
+            var total = numero * i;
+            System.out.printf("%s x %s = %s \n", numero, i, total);
+        }
+    }
+}
