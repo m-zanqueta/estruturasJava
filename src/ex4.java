1c8f6b03fce6c00f0812e4eda51762cf7b9caddd
Zanqueta <matheuszanqueta20@gmail.com>
2025-07-21 21:12:12 -0300

    Finalizando o quarto ex

--- src/ex4.java
+++ src/ex4.java
@@ -1,5 +1,29 @@
+import java.util.Scanner;
+
 public class ex4 {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        var scanner = new Scanner(System.in);
+        System.out.println("Divisíveis!");
+        System.out.println("--- \n");
+
+
+        System.out.println("Digite o primeiro número:");
+        var prim = scanner.nextInt();
+
+        while(true){
+            System.out.println("Digite o segundo número:");
+            var seg = scanner.nextInt();
+
+            if(seg > prim){
+                if((seg % prim) == 0){
+                System.out.println("São divisíveis!");
+                break;
+                }
+
+                System.out.println("Não são divisíveis! \n");
+            } else {
+                System.out.println("O segundo número deve ser maior que o primeiro! \n");
+            }
+        }
     }
 }
